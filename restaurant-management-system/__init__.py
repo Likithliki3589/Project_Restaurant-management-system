f24f772400ef2b7e262443f80090afceba481d93
likith_m <likithliki3589@gmail.com>
2025-06-11 12:23:49 +0530

    update

---
+++ restaurant-management-system/__init__.py
@@ -0,0 +1,1 @@
+from . import restaurant_management_system
---
+++ restaurant-management-system/__mainfest__.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Odoo, Open Source Management Solution
+#    Copyright (C) 2025 ZestyBeanz Technologies(<http://www.zbeanztech.com>)
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+{
+    'name': 'restaurant_management_system',
+	'version': '18.0.1.0',
+	'summary': 'This Module for training purpose',
+	'description': """This Module for training purpose.	""",
+	'category':'Learning',
+    'sequence': 100,
+	'author': 'Likith M',
+	'website': 'www.zbeanztech.com',
+	"license": "LGPL-3",
+	'depends': [],
+	'data': [ ],
+	'test': [],
+	'demo': [],
+	'installable': True,
+	'autoinstall': False,
+	'application': False,
+
+}
